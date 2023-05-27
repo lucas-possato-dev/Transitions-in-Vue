4d734202522d9e2025025701d6b4351047655135
lucas-possato-dev <lucaspstinho@gmail.com>
2023-05-27 09:53:26 -0300

    first transitions

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="Placeholder" />
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti enim
+      repudiandae odit eos inventore magni?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quisquam
+      suscipit quaerat enim corrupti non consequatur earum, eligendi quidem
+      ipsum vero facilis modi rerum nihil illo quos, adipisci ipsam provident.
+    </p>
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nisi.</p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores hic
+      incidunt sequi vero odio illum suscipit delectus rerum. Neque, maxime et!
+      Voluptas reiciendis molestias earum.
+    </p>
+  </main>
+</template>
